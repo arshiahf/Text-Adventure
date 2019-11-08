bfccf9198bea3afffbe9718ebc94a7fcc26ae4a4
Matthew Taurone <arshiahf@gmail.com>
2019-11-08 02:29:20 -0800

    Imported rest of initial room configuration from repl.it.

--- __main__.py
+++ __main__.py
@@ -1,1 +1,23 @@
+import json
 import room
+
+rooms_file = open("rooms.json", "r")
+rooms_json = json.load(rooms_file)
+
+rooms = dict()
+
+for current_room in rooms_json:
+  container = rooms_json[current_room]
+  descriptions = container["Description"]
+  commands = container["Commands"]
+  rooms[current_room] = room.room(current_room, descriptions, commands)
+
+current = "Home"
+
+print(rooms[current])
+while True:
+  current, command = rooms[current].command_rules(current)
+  if command != None:
+    print(command)
+  else:
+    print(rooms[current])
--- room.py
+++ room.py
@@ -16,3 +16,23 @@ class room:
             return self.choose_description(None, description[command])
         else:
             return description
+
+    def command_rules(self, current_room):
+        player_input = input("> ")
+
+        for command in self.commands:
+          if player_input.lower().capitalize() in self.commands and player_input.lower() == command.lower() and (player_input.lower() == "north" or player_input.lower() == "south" or player_input.lower() == "east" or player_input.lower() == "west" or player_input.lower() == "up" or player_input.lower() == "down"):
+            os.system("clear")
+            return self.commands[command], None
+          elif player_input.lower() == command.lower() and player_input.lower() != "Default":
+            os.system("clear")
+            self.description = self.choose_description(command, self.description)
+            print(self)
+            if type(self.commands[command]) == dict:
+              self.commands = self.commands[command]
+              return current_room, self.commands["Default"]
+            return current_room, self.commands[command]
+
+        os.system("clear")
+        print(self)
+        return current_room, "You can't do that."
